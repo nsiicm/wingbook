ee4e71e3d0ff63965080fe2c9f2e38559e20cd2d
Nicolas SIMON <nsimon@free-mobile.fr>
2025-05-21 12:37:47 +0200

    fix env vatiable

--- front/src/axios.js
+++ front/src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.API_BASE_URL || 'http://localhost:8012/api/', // your Django API base URL
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8012/api/'
 });
 
 export default api;
